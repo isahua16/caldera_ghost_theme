23b0cc461f05669f8c52512c769da5cd2c7ed10a
isahua16 <isael.huard@calderainteractive.com>
2023-11-16 20:39:29 -0700

    more css

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,9 +4,18 @@
 import "../css/index.css";
 
 // Import JS
-import menuOpen from "./menuOpen";
 import infiniteScroll from "./infiniteScroll";
 
 // Call the menu and infinite scroll functions
-menuOpen();
 infiniteScroll();
+
+function open_nav() {
+    let nav = document.querySelector(`#mobile_nav`);
+    nav.style.paddingBottom = `40px`;
+    nav.style.visibility = `visible`;
+    nav.style.opacity = `1`;
+    nav.style.height = `200px`;
+}
+
+let menu = document.querySelector(`#menu_icon`);
+menu.addEventListener(`click`, open_nav);
